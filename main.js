208c7b12f0fcd2944638ce57e9738a4fedd4da15
Hkrays <108366254+Hkrays@users.noreply.github.com>
2024-04-11 14:31:15 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
+
 const tempoObjetivo1 = new Date("2024-11-22T00:00:00");
+const tempoObjetivo2 = new Date("2024-11-22T00:00:00");
+const tempoObjetivo3 = new Date("2024-11-22T00:00:00");
+const tempoObjetivo4 = new Date("2024-11-22T00:00:00");
 
-contadores[0].textContent = calculatempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-function calculatempo(tempoObjetivo){
+
+
+function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal/1000);
@@ -16,20 +22,37 @@ function calculatempo(tempoObjetivo){
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-    
-    
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 
+    if (tempoFinal>0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    }else{
+        return "Prazo Finalizado";
+    }    
 }
 
-for (let i=0; i < botoes.length; i++){
+
+for (let i=0; i<botoes.length; i++){
     botoes[i].onclick=function(){
+
         for(let j=0; j<botoes.length; j++){
             botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo");
-        }
-
-        botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
+            textos[j].classList.remove("ativo");   
     }
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
+    }
+ }
+
+ function atualizaCronometro(){
+    for (let i=0; i<contadores.length; i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+ }
+
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
 }
+
+comecaCronometro();
